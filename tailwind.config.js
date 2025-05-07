8a8fbece23b20b83f0d722e1e038fe7ad3b08029
Ariel Poler <ariel@mastil.com>
2025-05-07 14:05:58 -0700

    Optimize GPT response with streaming and early TTS trigger

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,15 +11,15 @@ module.exports = {
       colors: {
         background: '#001f2f',
         'background-top': '#004050',
-        'box-accent': '#344e5c',
-        'dropdown-bg': '#2d3e3e',
+        'box-accent': '#67937e',
+        'dropdown-bg': '#67937e',
         'button-primary': '#c25e2c',
         'button-hover': '#9b451e',
         'voice-button': '#d6b56c',
         heading: '#d6b56c',
         copy: '#ffffff',
         'copy-soft': '#f5f5f5',
-        border: '#d9d9d9',
+        border: '#d9d9d9'
       },
       fontFamily: {
         heading: ['"Cinzel Decorative"', 'serif'],
