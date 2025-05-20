1ffeba9d323dd836c65c345740201abb9b458415
Ariel Poler <ariel@mastil.com>
2025-05-20 11:31:01 -0700

    Optimize GPT response with streaming and early TTS trigger

--- pages/api/auto-continue.js
+++ pages/api/auto-continue.js
@@ -1,3 +1,4 @@
+import { personas } from "../lib/personas"
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
       return res.status(405).json({ error: 'Method not allowed' });
