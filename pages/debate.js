bd4840a48d4ebe689ddbe86f2cc498aec5f75182
Ariel Poler <ariel@mastil.com>
2025-05-22 09:47:23 -0700

    Optimize GPT response with streaming and early TTS trigger

--- pages/debate.js
+++ pages/debate.js
@@ -1,15 +1,5 @@
 import { DebateInterface } from "../components/debate-interface"
-import Head from "next/head"
 
 export default function DebatePage() {
-  return (
-    <>
-      <Head>
-        <title>Historical Debates</title>
-        <meta name="description" content="Engage with historical figures in fascinating debates" />
-      </Head>
-      
-      <DebateInterface />
-    </>
-  )
+  return <DebateInterface />
 }
